aeaaf887c88549c3cabc8be639788fc82919ae09
oh-hi-yo <qaz80018@gmail.com>
2020-10-06 21:20:58 +0800

    adjust minPriorityQueue source code

--- minPriorityQueue.py
+++ minPriorityQueue.py
@@ -1,7 +1,7 @@
 class minPriorityQueue:
-  # implement a minimmum priority queue by min heap
+    # implement a minimmum priority queue by min heap
     def __init__(self, array=[]):
-        self.heap = list([None])  # None for the array[0]
+        self.heap = list([None])  # None for the array[0], root = array[1]
         self.length = 0
         if len(array) != 0:
             for item in array:
@@ -15,41 +15,41 @@ class minPriorityQueue:
         self.__swim(self.length)
         return
 
-    def extractMax(self):
+    def extractMin(self):
         # exchange first item and last item, then order again
-        maximum = self.heap[1]
-        self.__exchange(1, self.length),
-        self.heap.pop()
+        minimum = self.heap[1]
+        self.__exchange(1, self.length)
+        self.heap.pop()  # pop() 函数用于移除列表中的一个元素（默认最后一个元素），并且返回该元素的值。
         self.length = self.length - 1
         self.__sink(1)
 
-        return maximum
+        return minimum
 
     # __swim() 等前面有兩個底線的函數，也像是 C 或 Java 等語言中的 private 作用，能夠限制函數只能在類別中被呼叫
     def __swim(self, k):
         # Exchange key in child with key in parent. Repeat until heap order restored.
-        while k > 1 and self.__less(k // 2, k):
-        self.__exchange(k // 2, k)
-        k = k // 2
+        while k > 1 and self.__more(k // 2, k):
+            self.__exchange(k // 2, k)
+            k = k // 2
         return
 
     def __sink(self, k):
-        # Key in parent exchange with ket in larger child. Repeat until heap order restored.
+        # Key in parent exchange with key in smaller child. Repeat until heap order restored.
         while k * 2 <= self.length:
-        j = k * 2
-        if j < self.length and self.__less(j, j + 1):
-            j = j + 1
+            j = k * 2
+            if j < self.length and self.__more(j, j + 1):
+                j = j + 1
 
-        self.__exchange(k, j)
-        k = j
+            self.__exchange(k, j)  # exchange with the smallest child
+            k = j
         return
 
-    def __less(self, a, b):
-        # return a boolean represent if item at a position is less than item at b
-        if self.heap[a] < self.heap[b]:
-        return True
+    def __more(self, a, b):
+        # return a boolean represent if item at a position is larger than item at b
+        if self.heap[a] > self.heap[b]:
+            return True
         else:
-        return False
+            return False
 
     def __exchange(self, a, b):
         # swap item in the heap at position a and b
