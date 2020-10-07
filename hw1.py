109f833a805861865bbf30c0a90f99af5bc2998f
oh-hi-yo <qaz80018@gmail.com>
2020-10-07 14:41:53 +0800

    finish the excel Input

--- hw1.py
+++ hw1.py
@@ -1,8 +1,14 @@
+import pandas as pd
+from minPriorityQueue import minPriorityQueue as mPQ
 
 
 def main():
-    print("hello world")
-    print("NCTU")
+    # header ：指定作為列名的行，預設0，即取第一行，資料為列名行以下的資料；若資料不含列名，則設定 header = None；
+    df = pd.read_excel('test instance.xlsx', header=None)
+    process_time = df.iloc[0, 1:].tolist()
+    print(process_time)
+    arrival_time = df.iloc[1, 1:].tolist()
+    print(arrival_time)
 
 
 main()
